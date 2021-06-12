0ba0dd2f31d80286bea009cbffdcdbdd7ab51518
Дмитрий Морозов <mr.mirocow@gmail.com>
2021-06-12 18:22:19 +0300

    Added namespace for threads tests

---
+++ src/Threads.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace tests;
+
+class Threads
+{
+    private $queue;
+
+    public function __construct(&$queue)
+    {
+        $this->queue = &$queue;
+        $this->forks = [];
+    }
+
+    public function runAsync ($forks)
+    {
+        $lead = array_shift($this->queue);
+        $pid   = pcntl_fork();
+        if ($pid == -1) {
+            die('could not fork');
+        }
+        else if ($pid) {
+            $forks -= 1;
+            if ($forks > 0) {
+                $this->runAsync($forks);
+            }
+            pcntl_wait($status);
+        }
+        else {
+            asyncCall($lead);
+            posix_kill(getmypid(), SIGKILL);
+        }
+    }
+
+}
--- test_pcntl.php
+++ test_pcntl.php
@@ -19,43 +19,11 @@ function asyncCall($lead){
     return true;
 }
 
-class Threads
-{
-    private $queue;
-
-    public function __construct(&$queue)
-    {
-        $this->queue = &$queue;
-        $this->forks = [];
-    }
-
-    public function runAsync ($forks)
-    {
-        $lead = array_shift($this->queue);
-        $pid   = pcntl_fork();
-        if ($pid == -1) {
-            die('could not fork');
-        }
-        else if ($pid) {
-            $forks -= 1;
-            if ($forks > 0) {
-                $this->runAsync($forks);
-            }
-            pcntl_wait($status);
-        }
-        else {
-            asyncCall($lead);
-            posix_kill(getmypid(), SIGKILL);
-        }
-    }
-
-}
-
 @unlink(LOG_FILE_PATH);
 
 $start = microtime(true);
 while(count($queue) > 0) {
-    $threads = new Threads($queue);
+    $threads = new \tests\Threads($queue);
     $threads->runAsync(500);
 }
 $finish = microtime(true);
